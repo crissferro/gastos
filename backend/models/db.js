b4aef519e8a5c41cfe4fe01d67f320795b8c59b0
crissferro <crissferrogmail.com>
2025-03-05 19:26:25 -0300

    modificacion conexion db

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 
+
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
@@ -10,6 +11,19 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     queueLimit: 0
 });
+
+/*
+const pool = mysql.createPool({
+    host: '192.168.1.17',
+    user: 'root',
+    password: 'Luca2014',
+    database: 'gestor_gastos',
+    port: 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+*/
 
 module.exports = {
     conn: pool.promise()
